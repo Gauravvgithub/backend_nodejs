fc17b5a0c7369788b47ade88958b31e8315632b5
Gauravvgithub <gauravraj7011@gmail.com>
2025-05-03 15:20:23 +0530

    server side rendering|nodejs

--- NodeJS/class_02/index.js
+++ NodeJS/class_02/index.js
@@ -5,8 +5,33 @@ const http = require("http")
 const PORT = 3000
 
 const server = http.createServer((request, response)=>{
-    response.write("hello from server")
-    response.end()
+    // console.log(request.url)
+    // response.write("hello from server")
+    // response.end()
+    if(request.url === '/'){
+        fs.readFile(path.join(__dirname, "public", "index.html"), function(error, data){
+            response.writeHead(200,{"content-type":"text/html"})
+            response.end(data)
+        })
+    }
+    else if(request.url === '/about'){
+        fs.readFile(path.join(__dirname, "public", "about.html"), function(error, data){
+            response.writeHead(200,{"content-type":"text/html"})
+            response.end(data)
+        })
+    }
+    else if(request.url === '/contact'){
+        fs.readFile(path.join(__dirname, "public", "contact.html"), function(error, data){
+            response.writeHead(200,{"content-type":"text/html"})
+            response.end(data)
+        })
+    }
+    else if(request.url === '/custom/style.css'){
+        fs.readFile(path.join(__dirname, "public", "custom/style.css"), function(error, data){
+            response.writeHead(200,{"content-type":"text/css"})
+            response.end(data)
+        })
+    }
 })
 
 server.listen(PORT, function(){
